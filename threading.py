66df11e3d6419b0efeed86419109af56d1febf31
pelliy <pelinsuyildiz3@gmail.com>
2025-05-31 13:52:22 +0300

    threading.py

--- threading.py
+++ threading.py
@@ -5,16 +5,16 @@ sutun = 4
 matris= [[1,2,3,4],
          [1,2,3,4]]
 
-def coder(number):
-    print(f'Coder ID: {number}')
+def coder(number, pelin ):
+    print(f'Coder ID: {number}, {pelin}')
 
 
 threads = []
 for k in range(4):
     for j in range(4):
         t = threading.Thread(target=coder, args=(k,))
-    threads.append(t)
-    t.start()
+        threads.append(t)
+        t.start()
 
 for t in threads:
     t.join()
